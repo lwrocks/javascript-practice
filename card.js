e7f2ac0ba81244c290a10ea897494496c298cc0f
Laurel Wright <lwrocks@Laurels-Mac-mini.local>
2025-09-09 13:34:53 +0200

    Save and decrease buttons added

--- card.js
+++ card.js
@@ -2,11 +2,21 @@ let counter = 0;
 
 let countDisplay = document.getElementById("click-counter");
 
+let saveDisplay = document.getElementById("save-counter");
+
+// Function to increment the counter and update the display
 function incrementCounter() {
   counter += 1;
   countDisplay.textContent = counter;
 }
 
+function decreaseCounter() {
+  counter -= 1;
+  countDisplay.textContent = counter;
+}
+
+// Function to save the current counter value and update the save display
 function save() {
-  console.log("saved!");
+  let countStr = counter + " - ";
+  saveDisplay.textContent += countStr;
 }
